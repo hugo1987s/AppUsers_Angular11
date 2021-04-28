603658db4b1b2db72f77f1ad3888fff01b538a07
hugo1987s <hugo.schreiber@hasar.com>
2021-04-28 12:41:52 -0300

    Add GetUser method in userService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,12 +7,17 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
   url = 'https://gorest.co.in/public-api/users';
-  token = 'yourtoken';
+  token = 'bd9dce78ba784b0d145298835e49e24d5f3e5f5a96a2f63acdaa37a157229977';
 
   constructor(private http:HttpClient) { }
 
   getUsers() : Observable<any>
   {
       return this.http.get<any>(`${this.url}?access-token=${this.token}`);
+  }
+
+  getUser(userId:number) : Observable<any>
+  {
+    return this.http.get<any>(`${this.url}/${userId}?access-token=${this.token}`);
   }
 }
